344de4594299872acb422d2658db2cdfced1a751
Sebastian Köln <sebk@rynx.org>
2019-06-28 15:59:33 +0300

    .

--- pdf/src/lib.rs
+++ pdf/src/lib.rs
@@ -8,6 +8,15 @@
 #[macro_use] extern crate snafu;
 #[macro_use] extern crate log;
 
+extern crate num_traits;
+extern crate inflate;
+extern crate itertools;
+extern crate memmap;
+extern crate tuple;
+extern crate chrono;
+extern crate once_cell;
+extern crate tempfile;
+
 #[macro_use] pub mod error;
 //mod macros;
 pub mod object;
