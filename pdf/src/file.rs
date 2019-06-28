a609160137763a2fab87a881d5153bf593d26505
Sebastian Köln <sebk@rynx.org>
2019-06-28 15:59:13 +0300

    .

--- pdf/src/file.rs
+++ pdf/src/file.rs
@@ -68,22 +68,6 @@ impl<B: Backend> File<B> {
         })
     }
 
-    pub fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>> {
-        const PADDING: [u8; 32] = [
-            0x28, 0xBF, 0x4E, 0x5E, 0x4E, 0x75, 0x8A, 0x41,
-            0x64, 0x00, 0x4E, 0x56, 0xFF, 0xFA, 0x01, 0x08
-            0x2E, 0x2E, 0x00, 0xB6, 0xD0, 0x68, 0x3E, 0x80,
-            0x2F, 0x0C, 0xA9, 0xFE, 0x64, 0x53, 0x69, 0x7A
-        ];
-
-        if let Some(ref e) = self.trailer.encrypt_dict {
-            match e.get("Filter").map(|s| s.as_str()) {
-                Some("Standard") => match e.get("V").unwrap {
-                    0 => bail!("undocumented encryption algorithm"),
-                    1 | 2 => 
-                    
-                
-    }
 
     pub fn get_root(&self) -> &Catalog {
         &self.trailer.root
